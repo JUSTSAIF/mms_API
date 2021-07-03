366b895ef6f0dd25689aa3bf984b331e6dc2c071
ꜱ ᴀ ɪ ꜰ <33282571+JUSTSAIF@users.noreply.github.com>
2021-07-03 13:19:20 +0300

    Update key.php

--- v1/key.php
+++ v1/key.php
@@ -1,9 +1,6 @@
 <?php
 
-$ivKey              = "17b27a92fb06bd22";
-$_SESSION['ivKey']  = "17b27a92fb06bd22";
-$encKey             = "164fb8af76ba2f39fb5b61a3517ed361";
-$_SESSION['encKey'] = "164fb8af76ba2f39fb5b61a3517ed361";
-
-
-// token:17b27a92fb06bd22:kUPU3wdhqyMVejayXDEnRCloeVH9yd0tPXRpDjJypZI=
+$ivKey              = "Key Here";
+$_SESSION['ivKey']  = "Key Here";
+$encKey             = "Key Here";
+$_SESSION['encKey'] = "Key Here";
